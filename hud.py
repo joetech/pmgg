cae196b1f9fa723eae892fca20ae1ba55b931c6e
Joe Colburn <joe@joetech.com>
2013-07-22 22:23:56 -0400

    testing HUD python

---
+++ hud.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import urwid
+import urwid.raw_display
+
+def main():
+    text_header = (u"Bitches love HUDs!")
+
+    def button_press(button):
+        frame.footer = urwid.AttrWrap(urwid.Text(
+            [u"Pressed: ", button.get_label()]), 'header')
+
+    #radio_button_group = []
+
+    blank = urwid.Divider()
+    listbox_content = [
+        blank
+        ]
+
+    header = urwid.AttrWrap(urwid.Text(text_header), 'header')
+    listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
+    frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header)
+
+    palette = [
+        ('body','white','black', 'standout'),
+        ('reverse','light gray','black'),
+        ('header','white','dark red', 'bold'),
+        ('important','dark blue','light gray',('standout','underline')),
+        ('editfc','white', 'dark blue', 'bold'),
+        ('editbx','light gray', 'dark blue'),
+        ('editcp','black','light gray', 'standout'),
+        ('bright','dark gray','light gray', ('bold','standout')),
+        ('buttn','black','dark cyan'),
+        ('buttnf','white','dark blue','bold'),
+        ]
+
+
+    screen = urwid.raw_display.Screen()
+
+    def unhandled(key):
+        if key == 'f8':
+            raise urwid.ExitMainLoop()
+
+    urwid.MainLoop(frame, palette, screen,
+        unhandled_input=unhandled).run()
+
+def setup():
+    main()
+
+if '__main__'==__name__ or urwid.web_display.is_web_request():
+    setup()
