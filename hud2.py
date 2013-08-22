6c60eba6d91beb3d9c74769297b9bc9e8437a704
Joe Colburn <joecolburn@quickenloans.com>
2013-08-21 20:10:09 -0400

    starting out

--- hud2.py
+++ hud2.py
@@ -7,19 +7,34 @@ curses.noecho() # Do not echo out keys pressed
 curses.cbreak() # Capture keys without requiring the ENTER key
 stdscr.keypad(1) # Capture cursor keys
 
-#begin_x = 20 ; begin_y = 7
-#height = 5 ; width = 40
-#win = curses.newwin(height, width, begin_y, begin_x)
+begin_x = 0
+begin_y = 0
+height = 5
+width = 40
+win = curses.newwin(height, width, begin_y, begin_x)
+sleep(1)
 
-pad = curses.newpad(100, 100)
-for y in range(0, 100):
-    for x in range(0, 100):
-        try: pad.addch(y,x, ord('a') + (x*x+y*y) % 26 )
-        except curses.error: pass
+def spinner():
+	spinnerChars = ['-','\\','|','/']
+	for ch in spinnerChars:
+		win.addch(ch)
+		win.refresh()
+		sleep(.1)
 
-        # Displays a section of the pad in the middle of the screen
-        pad.refresh( 0,0, 5,5, 20,75)
-        sleep(.003)
+for i in range(1,10):
+	spinner()
+
+# headPad = curses.newpad(100, 100)
+
+# def spinner():
+# 	spinnerChars = ['-','\\','|','/']
+# 	for ch in spinnerChars:
+# 		headPad.addch(ch)
+# 		headPad.refresh( 0,0, 5,5, 20,110)
+# 		sleep(.1)
+
+# for i in range(1,10):
+# 	spinner()
 
 # The next 4 lines are cleanup and shutdown
 curses.nocbreak()
