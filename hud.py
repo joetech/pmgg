544b8126e2590c49f65302f7d47a5738681c6c7b
Joe Colburn <joe@joetech.com>
2013-08-22 00:56:27 +0000

    merging stuff together

--- hud.py
+++ hud.py
@@ -1,49 +1,15 @@
 #!/usr/bin/python
 
 from time import gmtime, strftime, sleep
-import urwid
-import urwid.raw_display
-import time
 import subprocess
 import gmail 
 import os
 import RPi.GPIO as GPIO
+import curses
 
-def main():
-    text_header = (u"S to Speak | X to exit")
+    headerText = "S to Speak | X to exit"
     g = 0
 
-    def button_press(button):
-        frame.footer = urwid.AttrWrap(urwid.Text(
-            [u"Pressed: ", button.get_label()]), 'header')
-
-    blank = urwid.Divider()
-    listbox_content = [
-        blank
-        ]
-
-    header = urwid.AttrWrap(urwid.Text(text_header), 'header')
-    listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
-    frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header)
-
-    palette = [
-        ('body','white','black', 'standout'),
-        ('reverse','light gray','black'),
-        ('footer','white','black', 'bold'),
-        ('header','white','dark blue', 'bold'),
-        ('working','white','dark red', 'bold'),
-        ('important','dark blue','light gray',('standout','underline')),
-        ('editfc','white', 'dark blue', 'bold'),
-        ('editbx','light gray', 'dark blue'),
-        ('editcp','black','light gray', 'standout'),
-        ('bright','dark gray','light gray', ('bold','standout')),
-        ('buttn','black','dark cyan'),
-        ('buttnf','white','dark blue','bold'),
-        ]
-
-
-    screen = urwid.raw_display.Screen()
-        
     def logEvent(txt):
         f = open('log.txt', 'a')
         timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
@@ -51,40 +17,40 @@ def main():
         f.close()
         
     def voiceCommand():
-        frame.footer = urwid.AttrWrap(urwid.Text('Listening...'), 'working')
+        headerText = 'Listening...'
         p = subprocess.Popen(["./speech.sh", ""], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         out = out.strip(' \t\n\r')
         logEvent('VOICE - ' + out)
-        frame.footer = urwid.AttrWrap(urwid.Text(out), 'footer')
+	headerText = out
 	if out == 'later':
-            raise urwid.ExitMainLoop()
+            return 'exit'
 	elif out == 'take a photo':
-            time.sleep(1)
-            frame.footer = urwid.AttrWrap(urwid.Text('Smile while I take your picture'), 'footer')
+            sleep(1)
+            headerText = 'Smile while I take your picture'
             p = subprocess.Popen(["./image.sh", ""], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = p.communicate()
             out = out.strip(' \t\n\r')
 	elif out == 'menu':
-            time.sleep(1)
-            frame.footer = urwid.AttrWrap(urwid.Text('Commands:\nmenu: This menu\ntweet: Send a tweet\ncheck email: Retrieve new emails\ntake a photo: Takes a photo\nlater: Exits the HUD'), 'footer')
+            sleep(1)
+            headerText = 'Commands:\nmenu: This menu\ntweet: Send a tweet\ncheck email: Retrieve new emails\ntake a photo: Takes a photo\nlater: Exits the HUD'
 	elif out == 'check email':
-            time.sleep(1)
-            frame.footer = urwid.AttrWrap(urwid.Text('One moment while I fetch new email'), 'footer')
+            sleep(1)
+            headerText = 'One moment while I fetch new email'
             getMail()
-            # Now call the method to take a photo
 	elif out == 'tweet':
-            time.sleep(1)
-            frame.footer = urwid.AttrWrap(urwid.Text('Go ahead...'), 'footer')
+            sleep(1)
+            headerText = 'Go ahead...'
             tweetMsg()
             # Now call the method to take a photo
+        return headerText
 
     def tweetMsg():
         p = subprocess.Popen(["./speech-long.sh", ""], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         out = out.strip(' \t\n\r')
-        time.sleep(1)
-        frame.footer = urwid.AttrWrap(urwid.Text('I heard ' + out + '.  Confirm by saying Send.'), 'footer')
+        sleep(1)
+        headerText = 'I heard ' + out + '.  Confirm by saying Send.'
 
     def initEmail():
         global g
@@ -102,31 +68,75 @@ def main():
         for msg in mail:
             msg.fetch()
             inbox_txt += msg.fr + msg.subject + "\n"
-            frame.footer = urwid.AttrWrap(urwid.Text(inbox_txt), 'footer')
+            headerText = inbox_txt
 
-    def unhandled(key):
-        if key == 'x':
-            raise urwid.ExitMainLoop()
-        if key == 's':
-            voiceCommand()
-    
-    g = initEmail()
 
-    while 1==1:
-        if GPIO.input(17) == False:
-            logEvent('Button Press')
-            voiceCommand()
-        elif GPIO.input(17) == True:
-            logEvent('Open')
-        sleep(0.1);
 
-#    urwid.MainLoop(frame, palette, screen,
-#        unhandled_input=unhandled).run()
 
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.IN)
-    main()
 
-if '__main__'==__name__ or urwid.web_display.is_web_request():
-    setup()
+# Set up the curses window basics
+stdscr = curses.initscr() # Init the screen
+curses.noecho() # Do not echo out keys pressed
+curses.cbreak() # Capture keys without requiring the ENTER key
+stdscr.keypad(1) # Capture cursor keys
+
+# Create a new curses window instance
+begin_x = 0
+begin_y = 0
+height = 5
+width = 40
+win = curses.newwin(height, width, begin_y, begin_x)
+
+# Set up button listener
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
+
+# set up some fun animations
+spinnerArrow = [
+'      |',
+'>     |',
+'->    |',
+'-->   |',
+'--->  |',
+'----> |',
+' ----)|',
+' ---(-|',
+' --(--|',
+' -(---|',
+' <----|',
+'<---- |',
+'<---- |',
+'----  |',
+'---   |',
+'--    |',
+'-     |',
+'      |']
+spinnerThrob = ['.','o','0','o']
+spinnerStar = ['-','\\','|','+','/','+']
+
+def spinner(spinnerChars):
+	for ch in spinnerChars:
+		win.move(0,0)
+		win.addstr(ch)
+		win.refresh()
+		sleep(.3)
+
+for i in range(1,10):
+	spinner(spinnerArrow)
+
+# Initialize Gmail
+g = initEmail()
+
+# Begin main loop
+while 1==1:
+	if GPIO.input(17) == False:
+		logEvent('Button pressed')
+		voiceCommand()
+	# elif GPIO.input(17) == True:
+		# logEvent('Open')
+	sleep(0.1);
+
+curses.nocbreak()
+stdscr.keypad(0)
+curses.echo()
+curses.endwin()
