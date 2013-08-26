5682d886013f8d40b30ad3724b9df55ecec792f3
Joe Colburn <joe@joetech.com>
2013-08-26 00:48:06 +0000

    save git credentials

--- go.sh
+++ go.sh
@@ -5,5 +5,8 @@ setfont /usr/share/consolefonts/Lat15-TerminusBold20x10.psf.gz
 # Now start in the right directory
 cd /home/pi/pmgg
 
+# Make git remember credentials for a month
+git config --global credential.helper 'cache --timeout=2592000'
+
 # Finally, we start up the hud
 sudo python ./hud.py
